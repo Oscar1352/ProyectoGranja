5a322fb3ed224e09da4d1376e06048c9d5a649e8
Oscar Luna <carolina.151270@gmail.com>
2021-03-20 08:48:57 -0600

    first commit

--- src/Granja/Animales/Vaca.java
+++
@@ -1,7 +0,0 @@
-package com.mycompany.ejinterfacesclasesabstractas.animales;
-
-import com.mycompany.ejinterfacesclasesabstractas.Clases.Animal;
-import com.mycompany.ejinterfacesclasesabstractas.interfaces.Domestico;
-import com.mycompany.ejinterfacesclasesabstractas.interfaces.Omnivoro;
-public class Vaca extends Animales{
-}
